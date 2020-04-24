ce03029606e9f236c527f125a10052376fc7f9ea
gongtuigigi <cocm1324@gmail.com>
2020-04-24 23:19:07 +0900

    apollo tried

---
+++ fullstack/angular-apollo/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {ApolloModule, Apollo} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+import {AppComponent} from './app.component';
+import {ListComponent} from './list/list.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		ListComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		ApolloModule,
+		HttpLinkModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { 
+	constructor (
+		apollo: Apollo, 
+		httpLink: HttpLink
+	) {
+		apollo.create({
+			link: httpLink.create({uri: ''}),
+			cache: new InMemoryCache()
+		});
+	}
+}
---
+++ fullstack/angular-apollo/src/app/list/list.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Course, Query} from '../types';
+import gql from 'graphql-tag';
+
+@Component({
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+	
+	courses: Observable<Course[]>
+
+	constructor(
+		private apollo: Apollo
+	) { }
+
+	ngOnInit() {
+		this.courses = this.apollo.watchQuery<Query>({
+			query: gql`
+				query allCourses {
+					allCourses {
+						id
+						title
+						author
+						description
+						topic
+						url
+					}
+				}
+			`
+		}).valueChanges.pipe(
+			map(result => result.data.allCourses)
+		);
+	}
+}
